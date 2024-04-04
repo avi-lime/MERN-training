d15fed706266b7b8ec42137ad949112082f5fcd5
Avi <sa3198154@gmail.com>
2024-04-04 16:29:45 +0530

    Day 3 over

--- Day3/js_intro/script.js
+++ Day3/js_intro/script.js
@@ -1,2 +1,71 @@
-console.log('Hello');
+// var _ = 'Avinash';
 
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let x = 'b' + 'a' + + 'a' + 'a';
+// console.log(x);
+
+// let str = '12'
+// let age = 20
+
+// let userName = "Avi"
+// str = `My name is : ${userName}`
+
+// console.log(str);
+
+
+function print(str) {
+    console.log(str);
+}
+
+// let sum = (a = 0, b = 0) => {
+//     return a + b
+// }
+
+
+
+// print(sum(2, 3))
+// print("Avinash")
+
+
+// let a = 2
+
+// switch (a) {
+//     case 1:
+//         print("One")
+//         break;
+
+//     case 2:
+//         print("Two")
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const obj = new Object();
+// obj.name = "Avinash";
+// obj.age = 20;
+
+// const obj2 = {
+//     firstName: "Avinash",
+//     lastName: "Kumar",
+//     age: 20,
+//     fullName: function () {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// console.log(obj2.fullName())
+
+const obj = {
+    "firstName": "Avinash",
+    "lastName": "Singh",
+    "age": 20,
+}
+
+
+const x = prompt("Enter property: ")
+console.log(obj[x])
