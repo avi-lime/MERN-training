81b1f0bc59294244874e1f4b8a462045bfc6ae26
Avi <sa3198154@gmail.com>
2024-04-05 14:42:43 +0530

    Finished up Event handling, starting forms

--- Day4/part2/script.js
+++ Day4/part2/script.js
@@ -5,6 +5,25 @@ let divEl = document.querySelector("div");
 
 let pEl = document.querySelector("p:nth-of-type(2)");
 
+
 divEl.removeChild(pEl)
 
 // pEl.remove();
+
+let newChild = (e) => {
+    // let newEl = document.createElement("h1");
+    // newEl.textContent = "This is a new element";
+    // divEl.appendChild(newEl)
+    // e.target.remove();
+}
+
+let changeLocation = (e) => {
+
+    let x = Math.floor(Math.random() * innerWidth);
+    let y = Math.floor(Math.random() * innerHeight);
+    e.target.style.position = "absolute";
+    e.target.style.left = `${x - 20}px`;
+    e.target.style.top = `${y - 5}px`;
+
+
+}
