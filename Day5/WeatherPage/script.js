23a717f7307f4c5d0360b22466cc3570d7d64c1b
Avi <sa3198154@gmail.com>
2024-04-09 09:34:31 +0530

    Day 5 projects

--- Day5/WeatherPage/script.js
+++ Day5/WeatherPage/script.js
@@ -1,20 +1,46 @@
-const API_KEY = `CUXYVM2EVSQUN6SMZJGRWKRZ7`
+const WEATHER_API_KEY = `CUXYVM2EVSQUN6SMZJGRWKRZ7`
+const LOCATION_API_KEY = `e2b3aefd4b104180b64a482f73b35241`
 
+
+// check the unit selected and change accordingly
+const unitSelect = document.querySelector("#unit")
+let units = unitSelect.value === "C" ? "uk" : "us"
+
+unitSelect.addEventListener("change", () => {
+    units = unitSelect.value === "C" ? "uk" : "us"
+    getLocation()
+})
+
+// function to get current location based on the IP address
+async function getLocation() {
+    await fetch(`https://api.geoapify.com/v1/ipinfo?apiKey=${LOCATION_API_KEY}`)
+        .then(res => res.json())
+        .then(data => fetchDetails(data.city.name))
+}
+
+
+// get the location from the user and fetch the weather details
 const btn = document.getElementById("sub")
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
     let loc = document.getElementById("loc").value
     fetchDetails(loc)
 })
 
+// function to fetch the weather details
 function fetchDetails(location) {
-    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=uk&key=${API_KEY}&contentType=json`)
+    document.getElementById("city").innerText = location
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${units}&key=${WEATHER_API_KEY}&contentType=json`)
         .then(res => res.json())
         .then(data => renderUI(data))
 }
 
+// function to render the UI
 const root = document.getElementById("root")
 
 function renderUI(data) {
+    document.querySelector("#val").innerText = data.currentConditions.temp
+    document.querySelector(".unit").innerText = unitSelect.value
     root.innerHTML = ""
     const days = data.days;
     const row = document.createElement("tr")
@@ -29,7 +55,7 @@ function renderUI(data) {
     root.appendChild(row)
 
     days.forEach(day => {
-        let date = day.datetime
+        let date = new Date(day.datetime).toDateString()
         let min = day.tempmin
         let max = day.tempmax
         let dataArr = [date, min, max]
@@ -43,8 +69,7 @@ function renderUI(data) {
         root.append(row)
     })
 
-
-
-
 }
 
+// get the location on load
+document.addEventListener("load", getLocation())
