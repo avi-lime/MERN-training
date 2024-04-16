477d44f24cefd932f37ad67aaa717eea9c1da3b8
Avi <sa3198154@gmail.com>
2024-04-16 16:27:56 +0530

    Done with Day 9

---
+++ Day9/index.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const fs = require('fs/promises');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use(express.json());
+
+async function fetchProducts({ id = null, name = null }) {
+    const res = await fetch("http://localhost:1010/api/products");
+    const data = await res.json();
+    const products = data.products
+
+    if (id) return products.find((product) => product.id == id)
+
+    if (name) return products.filter((product) => product.title.toLowerCase().includes(name.toLowerCase()));
+
+    return products;
+
+}
+
+app.get('/products', async (req, res) => {
+    const { id, name } = req.query;
+    console.log()
+    const products = await fetchProducts({ id, name })
+    res.json(products)
+});
+
+app.get('/api/products', (req, res) => {
+
+
+    fs.readFile('./data.json', 'utf-8')
+        .then((data) => {
+            res.status(200);
+            res.json({
+                status: 'success',
+                results: JSON.parse(data).length,
+                products: JSON.parse(data)
+            });
+        }).catch((err) => {
+            res.json({
+                status: 'error',
+                message: err.message
+            });
+        });
+});
+
+app.post('/api/products', async (req, res) => {
+    const products = await fetchProducts({});
+    const data = req.body;
+    if (!data.price || !data.title) {
+        res.json({
+            status: 'error',
+            message: 'Please provide title and price'
+        })
+        return
+    }
+    data.id = products.length == 0 ? 1 : products[products.length - 1].id + 1;
+    products.push(data);
+    fs.writeFile('./data.json', JSON.stringify(products))
+    res.end("Done")
+});
+
+app.put('/api/products/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const products = await fetchProducts({});
+    const data = req.body;
+    data.id = parseInt(id);
+    const newArray = products.map((product) => {
+        if (product.id == id) return data;
+        return product
+    })
+
+    fs.writeFile('./data.json', JSON.stringify(newArray))
+    res.status(200);
+    res.json({
+        status: 'success',
+        data: {
+            message: 'Product updated successfully',
+            product: data
+        }
+    });
+})
+
+app.delete('/api/products/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const products = await fetchProducts({});
+    const newArray = products.filter((product) => product.id != id);
+    fs.writeFile('./data.json', JSON.stringify(newArray))
+    res.status(204);
+    res.json({
+        status: 'success',
+        data: {
+            message: 'Product deleted successfully'
+
+        }
+    });
+})
+
+app.listen(1010, (req, res) => {
+    console.log('Server is running on port 1010');
+})
